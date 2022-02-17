574a9925d1d66517da03c79ad192090e4fddda34
Dina Rosita <dina@koudys.com>
2022-02-17 14:47:16 -0500

    Edit hover element js

--- hoverElement.js
+++ hoverElement.js
@@ -12,39 +12,28 @@ function assignTitle () {
 
         /* Title the second degree children */
         const genB = genA[i].children;
-        if (genB.length > 0) {
-            for (let j = 0; j < genB.length; j++) {
-                const genBName = genB[j].tagName;
-                const genBTitle = "<" + genBName.toLowerCase() + ">"; 
-                genB[j].title = genATitle + genBTitle;
+        for (let j = 0; j < genB.length; j++) {
+            const genBName = genB[j].tagName;
+            const genBTitle = "<" + genBName.toLowerCase() + ">"; 
+            genB[j].title = genATitle + genBTitle;
+            
+            /* Title the third degree children */
+            const genC = genB[j].children;
+            for (let k = 0; k < genC.length; k++) {
+                const genCName = genC[k].tagName;
+                const genCTitle = "<" + genCName.toLowerCase() + ">"; 
+                genC[k].title = genATitle + genBTitle + genCTitle;
                 
-                /* Title the third degree children */
-                const genC = genB[j].children;
-                if (genC.length > 0) {
-                    for (let k = 0; k < genC.length; k++) {
-                        const genCName = genC[k].tagName;
-                        const genCTitle = "<" + genCName.toLowerCase() + ">"; 
-                        genC[k].title = genATitle + genBTitle + genCTitle;
-                        
-                        /* Title the fourth degree children */
-                        const genD = genC[k].children;
-                        if (genD.length > 0) {
-                            for (let l = 0; l < genD.length; l++) {
-                                const genDName = genD[l].tagName;
-                                const genDTitle = "<" + genDName.toLowerCase() + ">"; 
-                                genD[l].title = genATitle + genBTitle + genCTitle + genDTitle;
-                                
-                            }
-                        } else {
-                            /* Do nothing */
-                        }
-                    }
-                } else {
-                    /* Do nothing */
+                /* Title the fourth degree children */
+                const genD = genC[k].children;
+            
+                for (let l = 0; l < genD.length; l++) {
+                    const genDName = genD[l].tagName;
+                    const genDTitle = "<" + genDName.toLowerCase() + ">"; 
+                    genD[l].title = genATitle + genBTitle + genCTitle + genDTitle;
+                    
                 }
             }
-        } else {
-            /* Do nothing */
         }
     }    
 };
